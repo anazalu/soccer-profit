74e5b361dc05a9727d298ba86dccb282f4472c53
anazalu <you@example.com>
2024-01-07 12:08:10 +0200

    add handlers

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -1,46 +1,57 @@
 import Grid from '@mui/material/Grid';
-import { Box, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Button, Typography, Checkbox, FormControlLabel, TextField, Divider } from '@mui/material';
+import { Box, Typography, Checkbox, TextField, Divider } from '@mui/material';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 interface Match {
-    leagueId: number;
     matchId: number;
     teamA: string;
     teamB: string;
-    stake?: number;
-    step?: number;
+    isChecked: boolean;
+    stake: number;
+    step: number;
+    sell: number;
+    stepFreeRoll: number;
 }
 
 interface MatchCardProps {
     match: Match;
-    setStake: (matchId: number, stake: number) => void;
     showStuff: boolean;
+    onCheck: (matchId: number, isChecked: boolean) => void;
+    setStake: (matchId: number, stake: number) => void;
+    setStep: (matchId: number, step: number) => void;
+    setSell: (matchId: number, sell: number) => void;
+    setStepFreeRoll: (matchId: number, stepFreeRoll: number) => void;
 }
 
 export type { Match };
 
-export default function MatchCard({ match, setStake, showStuff }: MatchCardProps) {
+export default function MatchCard({ match, showStuff, onCheck, setStake, setStep, setSell, setStepFreeRoll }: MatchCardProps) {
+
     return (
         <Grid container spacing={3} margin={5}>
             <Box margin={1} >
                 <Grid container spacing={1} columnSpacing={1} rowSpacing={1} key={match.matchId}>
 
                     <Grid item xs={3}>
-                        <Typography minWidth={1500} variant="body1">{match.teamA} vs {match.teamB}</Typography>
+                        {showStuff ? (
+                            <Typography minWidth={1500} variant="body1">{match.teamA} vs {match.teamB}</Typography>
+                        ) : (
+                            <Typography minWidth={1500} variant="body1"></Typography>
+                        )}
                     </Grid>
-                    (if (showStuff) {
-                        <Grid item xs={1}>
-                            <Checkbox {...label} />
-                        </Grid>
-                    })
+                    <Grid item xs={1}>
+                        {showStuff && (
+                            <Checkbox {...label} checked={match.isChecked} onChange={(e) => onCheck(match.matchId, e.target.checked)} />
+                        )}
+                    </Grid>
 
                     <Grid item xs={1}>
                         <TextField
                             type="text"
                             label="Stake"
                             size="small"
-                            // defaultValue={1}
+                            variant={showStuff ? undefined : "filled"}
                             value={match.stake}
                             onChange={(e) => setStake(match.matchId, ~~e.target.value)}
                             InputLabelProps={{ shrink: true }}
@@ -49,11 +60,13 @@ export default function MatchCard({ match, setStake, showStuff }: MatchCardProps
 
                     <Grid item xs={1}>
                         <TextField
+                            // https://mui.com/base-ui/react-number-input/
                             type="number"
                             label="Step"
                             size="small"
-                            // value={step}
-                            // onChange={(e) => setStep(~~e.target.value)}
+                            variant={showStuff ? undefined : "filled"}
+                            value={match.step}
+                            onChange={(e) => setStep(match.matchId, ~~e.target.value)}
                             InputLabelProps={{ shrink: true }}
                         />
                     </Grid>
@@ -63,8 +76,9 @@ export default function MatchCard({ match, setStake, showStuff }: MatchCardProps
                             type="text"
                             label="Sell"
                             size="small"
-                            // value={sell}
-                            // onChange={(e) => setSell(~~e.target.value)}
+                            variant={showStuff ? undefined : "filled"}
+                            value={match.sell}
+                            onChange={(e) => setSell(match.matchId, ~~e.target.value)}
                             InputLabelProps={{ shrink: true }}
                         />
                     </Grid>
@@ -73,10 +87,11 @@ export default function MatchCard({ match, setStake, showStuff }: MatchCardProps
 
                         <TextField
                             type="number"
-
-                            id="outlined-basic" label="SFree" variant="outlined" size="small"
-                            // value={stepFreeRoll}
-                            // onChange={(e) => setStepFreeRoll(~~e.target.value)}                            
+                            label="SFree"
+                            size="small"
+                            variant={showStuff ? undefined : "filled"}
+                            value={match.stepFreeRoll}
+                            onChange={(e) => setStepFreeRoll(match.matchId, ~~e.target.value)}
                             InputLabelProps={{ shrink: true }}
                         />
                     </Grid>
--- src/components/MatchesContainer.tsx
+++ src/components/MatchesContainer.tsx
@@ -1,8 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import MatchCard, { Match } from "./MatchCard";
+
+interface MatchDTO {
+    leagueId: number;
+    matchId: number;
+    teamA: string;
+    teamB: string;
+}
+
+interface BetDTO {
+    matchId: number;
+    stake: number;
+    step: number;
+    sell: number;
+    stepFreeRoll: number;
+}
 
 interface MatchesContainerProps {
     leagueId: string;
@@ -10,27 +25,129 @@ interface MatchesContainerProps {
 
 function MatchesContainer({ leagueId }: MatchesContainerProps) {
     const queryClient = useQueryClient();
-    const { data, isLoading, isError, error } = useQuery<Match[]>(
+
+    const { data: matchDTOs, isLoading, isError, error } = useQuery<MatchDTO[]>(
         {
             queryKey: ['matches' + leagueId],
             queryFn: () => axios.get(`http://localhost:8080/api/matches/${leagueId}`).then((response) => response.data)
         }
     );
 
-    const [listOfStakes, setListOfStakes] = useState({});
-    const handleSetStake = (matchId: number, stake: number) => {
-        setListOfStakes({ ...listOfStakes, [matchId]: stake });
-        console.log(listOfStakes);
+    // const betMutation = useMutation((bets: BetDTO[]) =>
+    //     axios.post(`http://localhost:8080/api/matches/bet`, bets).then((response) => {
+    //         console.log(response);
+    //         // queryClient.invalidateQueries(['horses' + bet.raceId]);
+    //     })
+    // );
+
+    const [matches, setMatches] = useState<{ [key: number]: Match }>({});
+
+    useEffect(() => {
+        if (matchDTOs) {
+            const convertedMatches: { [key: number]: Match } = {};
+
+            matchDTOs.forEach((matchDTO) => {
+                convertedMatches[matchDTO.matchId] = {
+                    matchId: matchDTO.matchId,
+                    teamA: matchDTO.teamA,
+                    teamB: matchDTO.teamB,
+                    isChecked: false,
+                    stake: 0,
+                    step: 0,
+                    sell: 0,
+                    stepFreeRoll: 0,
+                };
+            });
+
+            setMatches(convertedMatches)
+        }
+    }, [matchDTOs]);
+
+    const handleBet = () => {
+        console.log("Bet.");
+        const bets: BetDTO[] = [];
+        Object.values(matches).forEach((match) => {
+            if (match.isChecked) {
+                const bet: BetDTO = {
+                    matchId: match.matchId,
+                    stake: match.stake,
+                    step: match.step,
+                    sell: match.sell,
+                    stepFreeRoll: match.stepFreeRoll
+                }
+                bets.push(bet);
+            }
+        })
+        console.log(...bets);
+
+        // betMutation.mutate(bets);
     }
 
-    const [defaultStake, setDefaultStake] = useState(1);
+    const [defaultStake, setDefaultStake] = useState(5);
     const handleSetDefaultStake = (_: number, stake: number) => {
         setDefaultStake(stake);
         console.log('Set default stakes to:', stake);
     }
 
+    const [defaultStep, setDefaultStep] = useState(2);
+    const handleSetDefaultStep = (_: number, step: number) => {
+        setDefaultStep(step);
+        console.log('Set default step to:', step);
+    }
+
+    const [defaultSell, setDefaultSell] = useState(50);
+    const handleSetDefaultSell = (_: number, sell: number) => {
+        setDefaultSell(sell);
+        console.log('Set default sell to:', sell);
+    }
+
+    const [defaultStepFreeRoll, setDefaultStepFreeRoll] = useState(3);
+    const handleSetDefaultStepFreeRoll = (_: number, stepFreeRoll: number) => {
+        setDefaultStepFreeRoll(stepFreeRoll);
+        console.log('Set default freeroll to:', stepFreeRoll);
+    }
+
     const handleClearAll = () => {
         console.log("Clear all.");
+    };
+
+    const handleOnCheck = (matchId: number, isChecked: boolean) => {
+        console.log("Check some boxes.");
+        let match: Match = matches[matchId];
+        match.isChecked = isChecked;
+        match.stake = defaultStake;
+        match.step = defaultStep;
+        match.sell = defaultSell;
+        match.stepFreeRoll = defaultStepFreeRoll;
+        setMatches({ ...matches, [matchId]: match });
+    };
+
+    const handleSetStake = (matchId: number, stake: number) => {
+        console.log("Stake");
+        let match: Match = matches[matchId];
+        match.stake = stake;
+        setMatches({ ...matches, [matchId]: match });
+    }
+
+    const handleSetStep = (matchId: number, step: number) => {
+        console.log("Step.");
+        let match: Match = matches[matchId];
+        match.step = step;
+        setMatches({ ...matches, [matchId]: match });
+    };
+
+    const handleSetSell = (matchId: number, sell: number) => {
+        console.log("Sell.");
+        let match: Match = matches[matchId];
+        match.sell = sell;
+        setMatches({ ...matches, [matchId]: match });
+    };
+
+    const handlesetStepFreeRoll = (matchId: number, stepFreeRoll: number) => {
+        console.log("SFree.");
+        let match: Match = matches[matchId];
+        match.stepFreeRoll = stepFreeRoll;
+        setMatches({ ...matches, [matchId]: match });
     };
 
     if (isLoading) {
@@ -45,21 +162,24 @@ function MatchesContainer({ leagueId }: MatchesContainerProps) {
         <Grid container spacing={3} margin={5} columns={12}>
             <Grid item xs={8}>
                 <Box sx={{ width: 1500 }}>
-                        <Stack alignContent={'right'}>
-                            <Button onClick={handleClearAll}>Clear all fields</Button>
-                        </Stack>
+                    <Stack alignContent={'right'}>
+                        <Button onClick={handleClearAll}>Clear all fields / Clear default fields...</Button>
+                    </Stack>
                 </Box>
 
-                <MatchCard match={{ leagueId: 0, matchId: 0, teamA: 'Default values', teamB: '', stake: defaultStake }} setStake={handleSetDefaultStake} showStuff={true} />
+                <MatchCard match={{ matchId: 0, teamA: '', teamB: '', isChecked: true, stake: defaultStake, step: defaultStep, sell: defaultSell, stepFreeRoll: defaultStepFreeRoll }}
+                    showStuff={false} onCheck={() => undefined}
+                    setStake={handleSetDefaultStake} setStep={handleSetDefaultStep} setSell={handleSetDefaultSell} setStepFreeRoll={handleSetDefaultStepFreeRoll} />
                 {
-                    data?.map((match: Match) => (
-                        <MatchCard key={match.matchId} match={match} setStake={handleSetStake} showStuff={false} />
+                    Object.values(matches)?.map((match: Match) => (
+                        <MatchCard key={match.matchId} match={match} showStuff={true}
+                            onCheck={handleOnCheck} setStake={handleSetStake} setStep={handleSetStep} setSell={handleSetSell} setStepFreeRoll={handlesetStepFreeRoll} />
                     ))
                 }
             </Grid>
 
             <Grid item xs={4}>
-                <Button variant="contained" size="large" >Bet</Button>
+                <Button variant="contained" size="large" onClick={handleBet} >Bet</Button>
             </Grid>
 
         </Grid >
